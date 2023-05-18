54aabd7262724c1ea15b0b3ad9f66568b1615cb6
MedTech TCRC <chuksedwin.ce@gmail.com>
2023-05-18 17:13:51 +0100

    fifth commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-2%)k0($x(bx0tgv@mnwsu01ned!7hce##nl_z!8$nz(6u11xkv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -60,12 +60,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
 
 
 # Password validation
